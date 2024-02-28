44a4b49fc3b1666b24246c13b3cb7c344bbe917b
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2024-02-28 10:30:34 -0300

    Add files via upload

---
+++ Banco de Dados II/SQLQuery1.sql
@@ -0,0 +1,85 @@
+﻿Create database Biblioteca
+go
+use Biblioteca
+go
+-- Tabelas
+Create Table Assunto(
+	sg_Assunto char(01) not null,
+	ds_Assunto varchar(40))
+go
+Create Table Editora(
+	cd_Editora int not null,
+	nm_Editora varchar(40))
+go
+Create Table Autor(
+	cd_Autor int not null,
+	nm_Autor varchar(40),
+	ds_EnderecoAutor varchar(50))
+go
+Create Table Livro(
+	cd_Livro int not null,
+	nm_Titulo varchar(30),
+	vl_Livro money,
+	dt_Lancamento datetime,
+	sg_Assunto char(01),
+	cd_Editora int)
+go
+-- Chaves Primarias
+Alter Table Assunto
+	Add Primary Key (sg_Assunto)
+go
+Alter Table Editora
+	Add Primary Key (cd_Editora)
+go
+Alter Table Autor
+	Add Primary Key (cd_Autor)
+go
+Alter Table Livro
+	Add Primary Key (cd_Livro)
+go
+-- Chaves Estrangeiras
+Alter Table Livro
+	Add Foreign Key (sg_Assunto)
+	References Assunto
+go
+Alter Table Livro
+	Add Foreign Key (cd_Editora)
+	References Editora
+go
+-- Registros
+Insert Into Assunto
+	Values 	('D','Drama'),
+		('A','Aventura'),
+		('C','Com�dia')
+go
+Insert Into Editora
+	Values 	(1,'Erica'),
+		(2,'Campus')
+go
+Insert Into Autor
+	Values 	(1,'MARIA DA SILVA','RUA DO GRITO, 45'),
+		(2,'ANDR� CARDOSO','AV. DA SAUDADE,325'),
+		(3,'TATIANA SOUZA','AV. BRASIL, 4011'),		
+		(4,'MARCO ANDRADE','RUA DO IMPERADOR, 778')
+go
+Insert Into Livro
+	(cd_Livro, nm_Titulo, cd_Editora, sg_Assunto, vl_Livro)
+	Values 	(1,'Mar em F�ria',1,'D',65.00),
+		(2,'O Aeroporto',2,'A',30.00),
+		(3,'Einstein',2,'D',4.50),
+		(4,'Mar sem fim',1,'A',58.00)
+go
+----------------------------------
+--Atividade de Banco de Dados II--
+----------------------------------
+Create Table Autor_Livro (
+	cd_AutorLivro int not null,
+
+)
+go
+Alter Table Autor_Livro
+	Add Primary Key (PK_AutorLivro)
+	Add Foreign Key (FK_Autor) 
+	REFERENCES Autor
+go
+Alter Table Aut
