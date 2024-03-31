37a30adf80d48099ebfe3589eeb15a9b18a24b47
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2024-03-31 19:13:37 -0300

    Update exec6.php

--- Tópicos Especiais para Internet/Manipulação de Arrays/exec6.php
+++ Tópicos Especiais para Internet/Manipulação de Arrays/exec6.php
@@ -1,4 +1,4 @@
 <?php
-$alunos = array("Maria Fernanda", "Michelle Baraçal", "Joao Pedroca");   
+$alunos = array("Maria Fernanda", "Michelle Baraçal", "Joao Pedro");   
 echo nl2br("Adoentados: \n");
 echo print_r(array_slice($alunos, -2));
